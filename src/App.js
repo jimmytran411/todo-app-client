513cbdf4b097c9cd1c898ef848b0748c1c3a02a4
An Tran <jimmytran411@gmail.com>
2021-01-04 21:27:59 +0200

    add Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import Landing from "./Landing";
+import { Login } from "./Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users/login">Login</Link>
+            </li>
+          </ul>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/users/login" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,71 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useHistory } from "react-router";
+import { config } from "./config";
 
-const Login = () => {
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let isUser, isLogin;
+  const history = useHistory();
+
+  const loginRequest = () => {
+    axios
+      .post(`${config.baseApi}/users/login`, {
+        email,
+        password,
+      })
+      .then((res) => {
+        let token = res.data.token;
+        localStorage.setItem("token", token);
+        const localToken = localStorage.getItem("token");
+        console.log(localToken);
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        isUser = false;
+      });
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <span>{!isUser ? "Something went wrong, please try again" : ""}</span>
+      <form
+        className="login-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          loginRequest();
+        }}
+      >
+        <div className="user-box">
+          <label htmlFor="email">
+            Email
+            <input
+              id="email"
+              value={email}
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div className="user-box">
+          <label htmlFor="password">
+            Password
+            <input
+              id="password"
+              type="password"
+              value={password}
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </label>
+        </div>
+
+        <button className="login-btn">Submit</button>
+      </form>
     </div>
   );
 };
-
-export default Login;
