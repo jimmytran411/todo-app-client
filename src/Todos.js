4c9846ff29b3951920623ee8fe10f888670d620e
An Tran <jimmytran411@gmail.com>
2021-01-12 11:45:30 +0200

    add delete button in /todos

--- src/Todos.js
+++ src/Todos.js
@@ -18,9 +18,9 @@ export const Todos = () => {
       .catch((err) => console.log(err));
   }, [getTodos, setTodoList]);
 
-  const onCompleteCheck = (e) => {
-    let id = e.target.id;
-    let completed = e.target.checked;
+  const onCompleteCheck = (event) => {
+    let id = event.target.id;
+    let completed = event.target.checked;
     axios
       .patch(
         `${config.baseApi}/todo/${id}`,
@@ -39,6 +39,24 @@ export const Todos = () => {
             } else {
               return item;
             }
+          });
+        });
+      })
+      .catch(console.error);
+  };
+
+  const onDeleteClick = (event) => {
+    let id = event.target.id;
+    axios
+      .delete(`${config.baseApi}/todo/${id}`, {
+        headers: {
+          "x-auth": getToken(),
+        },
+      })
+      .then((res) => {
+        setTodoList((prevTodoList) => {
+          return prevTodoList.filter((item) => {
+            return item._id !== res.data.todo._id;
           });
         });
       })
@@ -69,6 +87,9 @@ export const Todos = () => {
               <Link to={`/todo/${data._id}`}>
                 <button>Edit</button>
               </Link>
+              <button id={data._id} onClick={onDeleteClick}>
+                Delete
+              </button>
             </div>
           ))
         )}
